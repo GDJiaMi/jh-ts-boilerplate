a56bbb9221bd9f04448e6dcdbf761858791259a2
ivan <carney520@hotmail.com>
2017-08-22 15:27:10 +0800

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# jh-ts-boilerplate
+Typescript 库模板
+
+## 目录结构
+```
+├─src   # Typescript源代码目录
+├─lib   # Typescript 编译输出目录
+├─doc   # 文档目录
+| └─api # 自动生成的API文档
+├─dist  # npm 分发目录
+└─test  # 测试用例
+```
+
+## 使用
+```
+git clone <当前项目> my-project
+rm jh-ts-boilerplate 
+```
---
+++ setup.js
@@ -0,0 +1,64 @@
+const shell = require('shelljs')
+const { exec } = require('child_process')
+
+function cleanRepo() {
+  console.log('正在重置git版本库')
+  shell.rm('-rf', './git/')
+  checkNodeVersion()
+}
+
+function checkNodeVersion() {
+  shell.exec('node --version', function(code, stdout, stderr) {
+    if (code !== 0) {
+      console.log(stderr)
+      process.exit(1)
+    } else {
+      const nodeVersion = stdout && parseFloat(stdout.substring(1))
+      if (nodeVersion < 5) {
+        console.error('Node 版本过低(' + nodeVersion + ')')
+      } else {
+        initialNpm()
+      }
+    }
+  })
+}
+
+function initialNpm() {
+  const child = exec('npm init', (err, stdout, stdin) => {
+    if (err) {
+      console.error('npm 初始化失败')
+      process.exit(1)
+      return
+    } else {
+      checkYarn((code, stderr, stdout) => {
+        if (code !== 0) {
+          console.error('初始化失败')
+        } else {
+          console.error('初始化成功')
+          shell.rm('./setup.js')
+        }
+      })
+    }
+  })
+  process.stdin.pipe(child.stdin)
+  child.stdout.pipe(process.stdout)
+  child.stderr.pipe(process.stderr)
+}
+
+function checkYarn(cb) {
+  shell.exec('yarn --version', function(code, stdout, stderr) {
+    if (
+      code !== 0 ||
+      parseFloat(stdout) < 0.15 ||
+      process.env.USE_YARN === 'false'
+    ) {
+      // npm
+      shell.exec('npm install', cb)
+    } else {
+      // yarn
+      shell.exec('yarn install', cb)
+    }
+  })
+}
+
+cleanRepo()
