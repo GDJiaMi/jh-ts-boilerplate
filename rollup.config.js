20b3964023556a2645d22c33b1a5f1d696054400
ivan <carney520@hotmail.com>
2017-08-28 18:15:39 +0800

    add rollup banner & process.env.NODE_ENV

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonJS from 'rollup-plugin-commonjs'
 import uglify from 'rollup-plugin-uglify'
 import progress from 'rollup-plugin-progress'
+import replace from 'rollup-plugin-replace'
 import pkg from './package.json'
 import _ from 'lodash'
 
@@ -11,42 +12,81 @@ const RESOLVE_CONFIG = {
   jsnext: true,
   main: true,
 }
+const BANNER = `
+/**
+ * ${pkg.name} - ${pkg.version}
+ * ${pkg.homepage || ''}
+ * Copyright(c) ${new Date().getFullYear()}
+ * Author ${pkg.author}
+ * Licence ${pkg.license}
+ */
+`
 
 const UMD_NAME = _.upperFirst(_.camelCase(pkg.name))
 
 export default [
-  // commonJS
+  // commonJS for development
   {
     input: './lib/index.js',
     output: {
       file: './dist/index.js',
       format: 'cjs',
+      banner: BANNER,
     },
-    plugins: [progress(), resolve(RESOLVE_CONFIG), commonJS(), filesize()],
+    plugins: [
+      progress(),
+      replace({
+        // don't replace NODE_ENV, let Webpack or other Bundler to handle it.
+        'process.env.MODULE_TYPE': JSON.stringify('commonjs'),
+      }),
+      resolve(RESOLVE_CONFIG),
+      commonJS(),
+      filesize(),
+    ],
   },
-  // UMD
+  // ES6 for development
+  {
+    input: './lib/index.js',
+    output: {
+      file: './dist/index.module.js',
+      format: 'es',
+      banner: BANNER,
+    },
+    plugins: [
+      progress(),
+      replace({
+        // don't replace NODE_ENV, let Webpack or other Bundler to handle it.
+        'process.env.MODULE_TYPE': JSON.stringify('es6'),
+      }),
+      resolve(RESOLVE_CONFIG),
+      commonJS(),
+      filesize(),
+    ],
+  },
+  // UMD for browser/production
   {
     input: './lib/index.js',
     output: {
       file: './dist/index.umd.js',
       format: 'umd',
       name: UMD_NAME,
+      banner: BANNER,
     },
     plugins: [
       progress(),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('production'),
+        'process.env.MODULE_TYPE': JSON.stringify('umd'),
+      }),
       resolve(RESOLVE_CONFIG),
       commonJS(),
-      uglify(),
+      uglify({
+        output: {
+          // preserve banner
+          comments: /Copyright\(c\)/,
+        },
+      }),
       filesize(),
     ],
-  },
-  // ES6
-  {
-    input: './lib/index.js',
-    output: {
-      file: './dist/index.module.js',
-      format: 'es',
-    },
-    plugins: [progress(), resolve(RESOLVE_CONFIG), commonJS(), filesize()],
   },
 ]
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('hello world')
+/**
+ * entry
+ */
+if (process.env.NODE_ENV === 'development') {
+  console.log('hello world')
+}
